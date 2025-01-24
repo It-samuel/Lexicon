f40ff0db454121093788da1dde71ad66df1a71cb
Dr Xam <samuelhappiness360@gmail.com>
2025-01-24 18:10:46 +0100

    feat(Products card, Featured products): added the product card style

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Hero} from "./components/Hero"
+import { FeaturedProducts } from './components/FeaturedProducts'
+import { ProductCard } from '../../components/Elements/ProductCard'
 
 export const HomePage = () => {
   return (
     <main>
-        <div>Home</div>
+        <Hero />
+        <FeaturedProducts />
+       
     </main>
   )
 }
