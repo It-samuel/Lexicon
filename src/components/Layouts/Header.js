0d93efa9a29645a5ee19e0aabbb3b7aae6fc8d51
Dr Xam <samuelhappiness360@gmail.com>
2025-01-24 16:55:04 +0100

    feat(footer): added footer components

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,28 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header>
+    <header className="border-b-2">
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-          <a
+          <Link to="/" className="flex "
             href="https://flowbite.com"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
+            clLinkssName="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-8"
               alt=" Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap ">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap ml-3 dark:text-white ">
               Lexicon
             </span>
-          </a>
+          </Link>
           <div className="flex items-center ">
-            <span className="cursor-pointer text-xl text-gray-700 dark:text-black mr-5 bi bi-gear-wide-connected"></span>     
-            <span className="cursor-pointer text-xl text-gray-700 dark:text-black mr-5 bi bi bi-search"></span>     
-            <span className="cursor-pointer text-xl text-gray-700 dark:text-black mr-5 bi bi-cart-fill relative"></span>     
-            <span className="cursor-pointer text-xl text-gray-700 dark:text-black mr-5 bi bi-person-circle"></span>     
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"></span>     
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi bi-search"></span>     
+            <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
+                    <span className="text-2xl bi bi-cart-fill relative">
+                      <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">0</span>
+                    </span>                    
+            </Link>            
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-person-circle"></span>     
           </div>
         </div>
       </nav>
