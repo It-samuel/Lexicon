7ae96d930f67c81ca01ea74a26d1b0ea2bad4d8c
Dr Xam <samuelhappiness360@gmail.com>
2025-01-25 17:00:56 +0100

    feat(Products, FeaturedProducts): fetched products from the local json server

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
  
-export const ProductCard = () => {
+export const ProductCard = ({products}) => {
+  const {id, name, overview, poster, price, rating, best_seller} = products
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <Link to="/" className="relative">
-        <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
+      <Link to={`products/${id}`} className="relative">
+        {best_seller && <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
           Best Seller
-        </span>
-        <img className="rounded-t-lg w-full h-64" src="https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=60" alt="" />
+        </span>}
+        <img className="rounded-t-lg w-full h-64" src={poster} alt="poster" />
       </Link>
       <div className="p-5">
-        <Link to="/">
+        <Link to={`products/${id}`}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            The name
+            {name}
           </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
-            aliquam aliquid iure inventore ducimus, tempora obcaecati excepturi
-            alias, error ipsum non magni nisi esse. Dolore maiores reiciendis
-            qui hic dolorum.
+            {overview}
           </p>
           <div className="flex items-center my-2">
                 <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
@@ -32,7 +30,7 @@ export const ProductCard = () => {
 
             <p className="flex justify-between items-center">
                 <span className="text-2xl dark:text-gray-200">
-                    <span>$ <span>23</span></span>
+                    <span>$ <span>{price}</span></span>
                 </span>
                 <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button>
             </p>
