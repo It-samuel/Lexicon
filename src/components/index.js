c1757811b989a0a5542486ecf30322ec1733eec3
Dr Xam <samuelhappiness360@gmail.com>
2025-01-30 15:22:44 +0100

    feat(ScrollToTop): Add scroll to top

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {Footer} from './Layouts/Footer';
 
 export {ProductCard} from './Elements/ProductCard';
 export {Ratings} from './Elements/Ratings'
+export {ScrollToTop} from './Other/ScrollToTop'
