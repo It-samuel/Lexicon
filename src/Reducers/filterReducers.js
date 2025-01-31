0ce55cc3d01f3ca41a6670dde1e99c1dd194a9d7
Dr Xam <samuelhappiness360@gmail.com>
2025-01-31 20:29:31 +0100

    feat(Filter): basic structure for context and reducer

---
+++ src/Reducers/filterReducers.js
@@ -0,0 +1,26 @@
+export const filterReducer = (state, action) => {
+
+    switch(type){
+
+        case "PRODUCT_LIST":
+            return
+
+        case "SORT_BY":
+            return
+
+        case "RATINGS":
+            return
+
+        case "BEST_SELLER_ONLY":
+            return
+
+        case "ONLY_IN_STOCK":
+            return
+
+        case "CLEAR_FILTER":
+            return
+
+        default:
+            throw new Error("No Case")
+    }
+}
