a927be41e31e5ac5743165e112cc4a2224aaa383
Dr Xam <samuelhappiness360@gmail.com>
2025-02-04 20:17:21 +0100

    feat(login/register): added login and register UI

--- src/routes/Allroutes.js
+++ src/routes/Allroutes.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, ProductsList, ProductDetail } from "../pages";
+import { HomePage, ProductsList, ProductDetail, Login, Register } from "../pages";
 
 
 export const AllRoutes = () => {    
@@ -8,6 +8,9 @@ export const AllRoutes = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/products" element={<ProductsList />} />
             <Route path="/Products/:id" element={<ProductDetail />} />
+
+            <Route path="Login" element={<Login />} />
+            <Route path="Register" element={<Register />} />
         </Routes>
     )
 }
