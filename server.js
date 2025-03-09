b8a85ab78412a08a67c56c6ea1bb7da990959ba7
Dr Xam <samuelhappiness360@gmail.com>
2025-03-10 00:41:56 +0100

    server update

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// For any request that doesn't match a static file, send the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Frontend server listening on port ${PORT}`);
+});
