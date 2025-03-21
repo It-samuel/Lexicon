45f97822594513f1f786ab2396a941684b9ecc25
Dr Xam <samuelhappiness360@gmail.com>
2025-03-21 23:30:21 +0100

    testing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // For any request that doesn't match a static file, send the index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.js'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
